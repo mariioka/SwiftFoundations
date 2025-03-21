399b2d26955348673c552bc82b4135fd0ffe3e90
mariioka <125134709+mariioka@users.noreply.github.com>
2025-03-21 10:17:08 -0300

    edit

--- CodigosCores.swift
+++ CodigosCores.swift
@@ -1,3 +1,4 @@
+//Codigo Cor para o fundo do Card
 //dentro de PokemonCard(View)
 struct PokemonCardView: View {
     var body: some View {
@@ -7,4 +8,10 @@ struct PokemonCardView: View {
     }
 }
 
-
+//Codigo Cor para o fundo do Text: tipo
+//dentro de PokemonCard(View)
+    ForEach(pokemon.types, id: \.self) { type in    //for tipos
+        Text(type.rawValue.capitalized)             //Text(tipo)
+            .background(type.color)
+            .foregroundColor(.white)
+    }
